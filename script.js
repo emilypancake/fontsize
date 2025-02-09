dd078b34684d7502f1cdf96c0b95a4b3be903e14
Emily Pan <contact.emilypan@gmail.com>
2025-02-08 23:32:09 -0800

    Create script.js
    1. Changes font size when slider is changed

---
+++ script.js
@@ -0,0 +1,8 @@
+const slider = document.getElementById("myRange");
+const text = document.getElementById("text");
+
+function changeFontSize(){
+  text.style.fontSize = size + "px";
+}
+
+slider.addEventListener("input", changeFontSize());
