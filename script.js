7fff04e6b22cef37d9e66926c09f1e5ffa080236
Emily Pan <contact.emilypan@gmail.com>
2025-02-08 23:44:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
   
 const slider = document.getElementById("myRange");
 const text = document.getElementById("text");
+const fontSizeText = document.getElementById("fontSize");
 
 function changeFontSize(){
-  text.style.fontSize = size + "px"; 
+  text.style.fontSize = slider.value + "px"; 
+  fontSizeText.innerText = slider.value;
 }
+
 
 slider.addEventListener("input", changeFontSize());
