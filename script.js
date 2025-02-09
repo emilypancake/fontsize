2d00a3f0fcb8c999ed6d6e82c8772ae2807ebaad
Emily Pan <contact.emilypan@gmail.com>
2025-02-09 00:01:19 -0800

    attempt to fix null slider value
    added DOMContentLoaded to double sure js loads after html

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-
+document.addEventListener("DOMContentLoaded", function(){
+  const slider = document.getElementById("myRange");
+  const text = document.getElementById("text");
+  const fontSizeText = document.getElementById("fontSize");
   
-const slider = document.getElementById("myRange");
-const text = document.getElementById("text");
-const fontSizeText = document.getElementById("fontSize");
-
-function changeFontSize(){
-  text.style.fontSize = slider.value + "px"; 
-  fontSizeText.innerText = slider.value;
+  function changeFontSize(){
+    text.style.fontSize = slider.value + "px"; 
+    fontSizeText.innerText = slider.value;
+  }
+  
+  
+  slider.addEventListener("input", function(){
+    changeFontSize();
+  });
 }
 
-
-slider.addEventListener("input", function(){
-  changeFontSize();
-});
